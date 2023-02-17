6afafedf1c0118eb4ff2d7ee38240fe026c56cd7
PrincewillRf <resolute1000@gmail.com>
2023-02-16 17:07:08 -0800

    use puts to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-A program that uses puts to print a string
+ * Return:ends wit 0
+ */
+int main(void)
+{
+	puts("\" programming is like building a multilingual puzzle");
+	return 0;
+}
