495aa790856e3ebe85917b948fc54acd4380ef44
PrincewillRf <resolute1000@gmail.com>
2023-02-16 17:28:32 -0800

    4

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- prints a string fdollowed by a newv line
+ * return:0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
