2fd6753a78e3bfa7fec88bf915fd809154dde59d
PrincewillRf <resolute1000@gmail.com>
2023-02-21 16:22:57 -0800

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
  * main - main function
- *
- * Return : 0
+ *_putchar: print the alphabet putchar
+ * Return : always 0
  */
 
 int main(void)
 {
-	printf("_putchar");
-
+	_putcar('_');
+	_putchar'('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar("\n");
 	return (0);
 }
