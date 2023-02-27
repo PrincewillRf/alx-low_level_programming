610578cbf996104957f7debf02326cf2f6874a40
PrincewillRf <resolute1000@gmail.com>
2023-02-27 02:07:37 -0800

    task 2

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,6 +2,7 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as parameter
  *
+ * @n: pointer
  * Return: nothing
  */
 
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the value of two integers
+ *
+ * Return: nothing
+ * @t: input
+ * @a: input
+ * @b:input
+ */
+
+void swap_int(int *a, int *b)
+{	
+	int t;
+
+	t = *b;
+	*b = *a;
+	*a = t;
+}
