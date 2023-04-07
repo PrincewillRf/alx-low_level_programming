4f308110b098837a6f3dcc066f744a34393acb28
PrincewillRf <122823384+PrincewillRf@users.noreply.github.com>
2023-04-07 02:17:40 -0400

    Create 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits - number of bits you would need to flip
+ * to get from one number to another
+ * @n: parameter
+ * @m: destiny
+ * Return: nflips
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int ii, nflipss = 0;
+	unsigned long int jj = sizeof(unsigned long int) * 8;
+
+	for (i = 0; ii < jj; i++)
+	{
+		if ((m & 1) != (n & 1))
+			nflipss += 1;
+		n = n >> 1;
+		m = m >> 1;
+	}
+	return (nflipss);
+}
