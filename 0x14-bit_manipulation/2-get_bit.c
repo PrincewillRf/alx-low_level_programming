2593d498b4d51c533cf9900ce43e7561d0f03fef
PrincewillRf <122823384+PrincewillRf@users.noreply.github.com>
2023-04-08 05:40:23 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,15 +6,18 @@
  * Return: the value of the bit at index or -1 if error
 */
 
-/**
+
 int get_bit(unsigned long int n, unsigned int index)
 {
+        unsigned int ind;
+        ind = index;
+
 	if (index > 53)
 		return (-1);
 	return ((n >> index) & 1);
 }
 
-*/
+/**
 int get_bit(unsigned long int n, unsigned int index) {
     // Check that index is not out of range
     if (index >= sizeof(unsigned long int) * 8) {
@@ -29,4 +32,5 @@ int get_bit(unsigned long int n, unsigned int index) {
 
     return bit_value;
 }
+*/
 
